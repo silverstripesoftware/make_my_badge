0fe9141825dfefc66eb063a76095c1c793262879
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2011-07-30 17:41:11 +0530

    Added index page

--- make_my_badge/urls.py
+++ make_my_badge/urls.py
@@ -4,9 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'make_my_badge.views.home', name='home'),
-    # url(r'^make_my_badge/', include('make_my_badge.foo.urls')),
+    url(r'^$', 'badge.views.index', name='index'),
+#    url(r'^make_my_badge/', include('make_my_badge.foo.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
