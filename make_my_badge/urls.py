2b192776b68248bcd81a9af74a85d9fc6e6037f0
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2011-08-01 01:47:18 +0530

    Serving uploaded files in runserver mode

--- make_my_badge/urls.py
+++ make_my_badge/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
@@ -9,3 +10,11 @@ urlpatterns = patterns('',
     url(r'^badge/', include('badge.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # make django serve the uploaded files path in development
+    urlpatterns += patterns('',
+        url(r'^uploads/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
