328087b87d91bb7f20be85ef7063f6a75d1ad6c5
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2011-07-31 09:28:25 +0530

    Moved badge generation class from app to a python lib

--- make_my_badge/badgeGenerator/badgeApi.py
+++
@@ -1,9 +0,0 @@
-from createBadges import BadgeImage
-
-
-def make_badge(name,company,id):
-    badge = BadgeImage("badge_template.png")
-    badge.drawPerson(name)
-    badge.drawCompany(company)
-    badge.drawId(id)
-    return badge
