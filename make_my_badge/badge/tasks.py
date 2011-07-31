78e1a546812f9ddd92d2f5405e0625661b95cad3
kausikram krishnasayee <kausikram@silverstripesoftware.com>
2011-07-31 18:46:22 +0530

    task file did not get into the last commit

--- make_my_badge/badge/tasks.py
+++ make_my_badge/badge/tasks.py
@@ -1,7 +1,21 @@
-from celery.decorators import task
+import StringIO
 
-print "loaded"
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from celery.decorators import task
+from badge.models import BadgeTemplate
+from badge.badgeImage import BadgeImage
 
 @task()
-def add(x, y):
-    return x + y
+def generate_badge(item):
+    event = item.event
+    badge_template = BadgeTemplate.objects.filter(event=event)[0]
+    badge_img = BadgeImage(badge_template.template_image.path)
+    badge_img.drawPerson(item.name)
+    badge_img.drawCompany("Silver Stripe Software")
+    badge_img.drawId(str(item.id))
+    badge_io = StringIO.StringIO()
+    badge_img.img.save(badge_io,"png")
+    badge_file = InMemoryUploadedFile(badge_io, None, 'temp_file.png', 'image/png',badge_io.len, None)
+    item.generated_image.save("badge_image_%d.png"%(item.id,),badge_file)
+    item.save()
