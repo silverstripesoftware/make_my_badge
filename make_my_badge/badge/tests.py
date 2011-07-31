2f159a319e31f59c3d602c92e7f37108e9a0c576
kausikram krishnasayee <kausikram@silverstripesoftware.com>
2011-07-31 22:34:43 +0530

    view to run tasks to generatebadges for all items in an event

--- make_my_badge/badge/tests.py
+++ make_my_badge/badge/tests.py
@@ -6,11 +6,20 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.http import HttpRequest
+from django.contrib.auth.models import User
 
+from badge.models import Event, Item
+from badge.views import generate_badges
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class GenerateBadgeViewTest(TestCase):
+    def test_generate_badge_returns_ok(self):
+        u = User.objects.create_user("test","test@test.com","pass")
+        e = Event.objects.create(name="Test",user = u)
+        i = Item.objects.create(event=e,name="Blah",properties="{}")
+        request = HttpRequest()
+        request.GET = {}
+        response = generate_badges(request,e.id)
+        self.assertEquals(200,response.status_code)
+        self.assertEquals("application/json",response["Content-Type"])
+
--- make_my_badge/badge/views.py
+++ make_my_badge/badge/views.py
@@ -1,7 +1,10 @@
+import simplejson
 from django.template.response import TemplateResponse
 from django.views.generic.list_detail import object_list
+from django.http import HttpResponse
 
 from badge.models import Item
+from badge.tasks import generate_badge
 
 def index(request):
     return TemplateResponse(request, "index.html", {})
@@ -10,3 +13,14 @@ def index(request):
 def item_list(request,event_id):
     item_list = Item.objects.filter(event__id=int(event_id))
     return object_list(request, item_list, template_name="item_list.html", template_object_name="item")
+
+def generate_badges(request,event_id):
+    item_list = Item.objects.filter(event__id=int(event_id))
+    for item in item_list:
+        generate_badge.delay(item)
+    r = HttpResponse()
+    response_obj = {"statu":{"message":"ok"}}
+    r.content = simplejson.dumps(response_obj)
+    r["Content-Type"] = "application/json"
+    r.status_code = 200
+    return r
